3c115cf7cd03b41f75558c2e5ca877aa7954f314
adonm <adonm@fastmail.fm>
2021-06-01 11:21:32 +0800

    use a eventgrid trigger externally configured

--- ingest-azblob-to-pg/__init__.py
+++ ingest-azblob-to-pg/__init__.py
@@ -21,13 +21,13 @@ def init_database():
     db.connect()
     db.create_tables([LogEntry])
 
-def main(myblob: func.InputStream):
-    container, blobname = myblob.name.split("/", 1)
+def main(event: func.EventGridEvent):
+    container, blobname = event.subject.split("/containers/", 1)[1].split("/blobs/", 1)
     blobclient = BlobServiceClient.from_connection_string(os.environ["dbcafleetcaredata_STORAGE"])
     blob = blobclient.get_container_client(container).get_blob_client(blobname)
     blobprops = blob.get_blob_properties()
     blobtext = blob.download_blob().content_as_text()
-    LogEntry(name=myblob.name, created=blobprops.last_modified, text=blobtext).save()
+    LogEntry(name=f"{container}/{blobname}", created=event.event_time, text=blobtext).save()
     logging.info(f"Imported blob to database \n"
-                 f"Name: {myblob.name}\n"
+                 f"Name: {blobname}\n"
                  f"Time: \n {blobprops.last_modified}")
